06c4123bda709db4e9757f91eac523b4e86e5c0d
Le Hoai Nguyen Phuc <lehoainguyenphuc11b3@gmail.com>
2025-03-10 20:14:28 +0700

    Add Many Component

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,13 @@
+
+
+
+const MyComponent = () => {
+      return (
+            <div>
+                  <h1>Lê Hoài Nguyễn Phúc</h1>
+            </div>
+      );
+}
+
+
+export default MyComponent;
---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,20 @@
+
+const ThirdComponent = () => {
+      return (
+            <div>
+                  <h1>Third Component</h1>
+            </div>
+      );
+}
+
+
+const SecondComponent = () => {
+      return (
+            <div>
+                  <h1>Second Component</h1>
+            </div>
+      );
+}
+
+
+export { SecondComponent, ThirdComponent };
