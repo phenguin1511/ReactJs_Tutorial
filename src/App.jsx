a5ce641e44f8fdc79688e0b6bca41315414f9d27
Le Hoai Nguyen Phuc <lehoainguyenphuc11b3@gmail.com>
2025-03-12 10:37:09 +0700

    Props in ReactJs

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,19 @@ import "./App.css"
 
 const App = () => {
 
-
+  const name = "Nguyễn Phúc"
+  const data = {
+    address: "Biên Hòa",
+    phoneNumber: "0337326045"
+  }
   return (
     <>
       <div className="todo-container">
         <div className="todo-title">Todo List</div>
-        <TodoNew />
+        <TodoNew
+          name={name}
+          data={data}
+        />
         <div>Learning ReactJS</div>
         <img className="logo" src={reactLogo} alt="React Logo" />
 
--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,9 +1,16 @@
 import "./todo.css";
 import TodoList from "./TodoList";
 
-const TodoNew = () => {
+const TodoNew = (props) => {
+
+      const { data, name } = props;
       return (
             <>
+                  <div>
+                        <h1>{name}</h1>
+                        <p>{data.address}</p>
+                        <p>{data.phoneNumber}</p>
+                  </div>
                   <div className="todo-form">
                         <input type="text" className="todo-input" />
                         <button className="todo-button">Add</button>
